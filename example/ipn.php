5d681ae7fc9be9bf8df0cb1ad415f6b13e2ac7a9
Micah Carrick <micah@quixotix.com>
2011-09-09 06:41:48 -0700

    first commit

---
+++ example/ipn.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ *  PHP-PayPal-IPN Example
+ *
+ *  This shows a basic example of how to use the IpnListener() PHP class to 
+ *  implement a PayPal Instant Payment Notification (IPN) listener script.
+ *
+ *  https://github.com/Quixotix/PHP-PayPal-IPN
+ *
+ *  @package    PHP-PayPal-IPN
+ *  @author     Micah Carrick
+ *  @copyright  (c) 2011 - Micah Carrick
+ *  @license    http://opensource.org/licenses/gpl-3.0.html
+ */
+ 
+ 
+/*
+Since this script is executed on the back end between the PayPal server and this
+script, you will want to log errors to a file or email. Do not try to use echo
+or print--it will not work! 
+
+Here I am turning on PHP error logging to a file called "ipn_errors.log". Make
+sure your web server has permissions to write to that file. In a production 
+environment it is better to have that log file outside of the web root.
+*/
+ini_set('log_errors', true);
+ini_set('error_log', dirname(__FILE__).'/ipn_errors.log');
+
+
+// instantiate the IpnListener class
+include('../ipnlistener.php');
+$listener = new IpnListener();
+
+
+/*
+When you are testing your IPN script you should be using a PayPal "Sandbox"
+account: https://developer.paypal.com
+When you are ready to go live change use_sandbox to false.
+*/
+$listener->use_sandbox = true;
+
+
+/*
+The processIpn() method will encode the POST variables sent by PayPal and then
+POST them back to the PayPal server. An exception will be thrown if there is 
+a fatal error (cannot connect, your server is not configured properly, etc.).
+Use a try/catch block to catch these fatal errors and log to the ipn_errors.log
+file we setup at the top of this file.
+*/
+try {
+    $verified = $listener->processIpn();
+} catch (Exception $e) {
+    error_log('PayPal IPN: '.$e->getMessage());
+    exit(0);
+}
+
+
+/*
+The processIpn() method returned true if the IPN was "VERIFIED" and false if it
+was "INVALID".
+*/
+if ($verified) {
+    /*
+    Once you have a verified IPN you need to do a few more checks on the POST
+    fields--typically against data you stored in your database during when the
+    end user made a purchase (such as in the "success" page on a web payments
+    standard button). The fields PayPal recommends checking are:
+    
+        1. Check the $_POST['payment_status'] is "Completed"
+	    2. Check that $_POST['txn_id'] has not been previously processed 
+	    3. Check that $_POST['receiver_email'] is your Primary PayPal email 
+	    4. Check that $_POST['payment_amount'] and $_POST['payment_currency'] 
+	       are correct
+    
+    Since implementations on this varies, I will leave these checks out of this
+    example and just send an email using the getTextReport() method to get all
+    of the details about the IPN.  
+    */
+    mail('YOUR EMAIL ADDRESS', 'Verified IPN', $listener->getTextReport());
+
+} else {
+    /*
+    An Invalid IPN *may* be caused by a fraudulent transaction attempt. It's
+    a good idea to have a developer or sys admin manually investigate any 
+    invalid IPN.
+    */
+    mail('YOUR EMAIL ADDRESS', 'Invalid IPN', $listener->getTextReport());
+}
+
+?>
---
+++ ipnlistener.php
@@ -0,0 +1,261 @@
+<?php
+/**
+ *  PayPal IPN Listener
+ *
+ *  A class to listen for and handle Instant Payment Notifications (IPN) from 
+ *  the PayPal server.
+ *
+ *  https://github.com/Quixotix/PHP-PayPal-IPN
+ *
+ *  @package    PHP-PayPal-IPN
+ *  @author     Micah Carrick
+ *  @copyright  (c) 2011 - Micah Carrick
+ *  @version    2.0
+ *  @license    http://opensource.org/licenses/gpl-3.0.html
+ */
+class IpnListener {
+    
+    /**
+     *  If true, the recommended cURL PHP library is used to send the post back 
+     *  to PayPal. If flase then fsockopen() is used. Default true.
+     *
+     *  @var boolean
+     */
+    public $use_curl = true;     
+    
+    /**
+     *  If true, an SSL secure connection (port 443) is used for the post back 
+     *  as recommended by PayPal. If false, a standard HTTP (port 80) connection
+     *  is used. Default true.
+     *
+     *  @var boolean
+     */
+    public $use_ssl = true;      
+    
+    /**
+     *  If true, the paypal sandbox URI www.sandbox.paypal.com is used for the
+     *  post back. If false, the live URI www.paypal.com is used. Default false.
+     *
+     *  @var boolean
+     */
+    public $use_sandbox = false; 
+    
+    /**
+     *  The amount of time, in seconds, to wait for the PayPal server to respond
+     *  before timing out. Default 30 seconds.
+     *
+     *  @var int
+     */
+    public $timeout = 30;       
+    
+    private $post_uri = '';     
+    private $response_status = '';
+    private $response = '';
+
+    const PAYPAL_HOST = 'www.paypal.com';
+    const SANDBOX_HOST = 'www.sandbox.paypal.com';
+    
+    /**
+     *  Post Back Using cURL
+     *
+     *  Sends the post back to PayPal using the cURL library. Called by
+     *  the processIpn() method if the use_curl property is true. Throws an
+     *  exception if the post fails. Populates the response, response_status,
+     *  and post_uri properties on success.
+     *
+     *  @param  string  The post data as a URL encoded string
+     */
+    protected function curlPost($encoded_data) {
+
+        if ($this->use_ssl) {
+            $uri = 'https://'.$this->getPaypalHost().'/cgi-bin/webscr';
+            $this->post_uri = $uri;
+        } else {
+            $uri = 'http://'.$this->getPaypalHost().'/cgi-bin/webscr';
+            $this->post_uri = $uri;
+        }
+        
+        $ch = curl_init();
+        
+        curl_setopt($ch, CURLOPT_URL, $uri);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $encoded_data);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_TIMEOUT, $this->timeout);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, true);
+        
+        $this->response = curl_exec($ch);
+        $this->response_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    }
+    
+    /**
+     *  Post Back Using fsockopen()
+     *
+     *  Sends the post back to PayPal using the fsockopen() function. Called by
+     *  the processIpn() method if the use_curl property is false. Throws an
+     *  exception if the post fails. Populates the response, response_status,
+     *  and post_uri properties on success.
+     *
+     *  @param  string  The post data as a URL encoded string
+     */
+    protected function fsockPost($encoded_data) {
+    
+        if ($this->use_ssl) {
+            $uri = 'ssl://'.$this->getPaypalHost();
+            $port = '443';
+            $this->post_uri = $uri.'/cgi-bin/webscr';
+        } else {
+            $uri = $this->getPaypalHost(); // no "http://" in call to fsockopen()
+            $port = '80';
+            $this->post_uri = 'http://'.$uri.'/cgi-bin/webscr';
+        }
+
+        $fp = fsockopen($uri, $port, $errno, $errstr, $this->timeout);
+        
+        if (!$fp) { 
+            // fsockopen error
+            throw new Exception("fsockopen error: [$errno] $errstr");
+        } 
+
+        $header .= "POST /cgi-bin/webscr HTTP/1.0\r\n";
+        $header .= "Content-Type: application/x-www-form-urlencoded\r\n";
+        $header .= "Content-Length: ".strlen($encoded_data)."\r\n";
+        $header .= "Connection: Close\r\n\r\n";
+        
+        fputs($fp, $header.$encoded_data."\r\n\r\n");
+        
+        while(!feof($fp)) { 
+            if (empty($this->response)) {
+                // extract HTTP status from first line
+                $this->response .= $status = fgets($fp, 1024); 
+                $this->response_status = trim(substr($status, 9, 4));
+            } else {
+                $this->response .= fgets($fp, 1024); 
+            }
+        } 
+        
+        fclose($fp);
+    }
+    
+    private function getPaypalHost() {
+        if ($this->use_sandbox) return IpnListener::SANDBOX_HOST;
+        else return IpnListener::PAYPAL_HOST;
+    }
+    
+    /**
+     *  Get POST URI
+     *
+     *  Returns the URI that was used to send the post back to PayPal. This can
+     *  be useful for troubleshooting connection problems. The default URI
+     *  would be "ssl://www.sandbox.paypal.com:443/cgi-bin/webscr"
+     *
+     *  @return string
+     */
+    public function getPostUri() {
+        return $this->post_uri;
+    }
+    
+    /**
+     *  Get Response
+     *
+     *  Returns the entire response from PayPal as a string including all the
+     *  HTTP headers.
+     *
+     *  @return string
+     */
+    public function getResponse() {
+        return $this->response;
+    }
+    
+    /**
+     *  Get Response Status
+     *
+     *  Returns the HTTP response status code from PayPal. This should be "200"
+     *  if the post back was successful. 
+     *
+     *  @return string
+     */
+    public function getResponseStatus() {
+        return $this->response_status;
+    }
+    
+    /**
+     *  Get Text Report
+     *
+     *  Returns a report of the IPN transaction in plain text format. This is
+     *  useful in emails to order processors and system administrators. Override
+     *  this method in your own class to customize the report.
+     *
+     *  @return string
+     */
+    public function getTextReport() {
+        
+        $r = '';
+        
+        // date and POST url
+        for ($i=0; $i<80; $i++) { $r .= '-'; }
+        $r .= "\n[".date('m/d/Y g:i A').'] - '.$this->getPostUri();
+        if ($this->use_curl) $r .= " (curl)\n";
+        else $r .= " (fsockopen)\n";
+        
+        // HTTP Response
+        for ($i=0; $i<80; $i++) { $r .= '-'; }
+        $r .= "\n{$this->getResponse()}\n";
+        
+        // POST vars
+        for ($i=0; $i<80; $i++) { $r .= '-'; }
+        $r .= "\n";
+        
+        foreach ($_POST as $key => $value) {
+            $r .= str_pad($key, 25)."$value\n";
+        }
+        $r .= "\n\n";
+        
+        return $r;
+    }
+    
+    /**
+     *  Process IPN
+     *
+     *  Handles the IPN post back to PayPal and parsing the response. Call this
+     *  method from your IPN listener script. Returns true if the response came
+     *  back as "VERIFIED", false if the response came back "INVALID", and 
+     *  throws an exception if there is an error.
+     *
+     *  @return boolean
+     */    
+    public function processIpn() {
+        
+        if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+            header('Allow: GET', true, 405);
+            throw new Exception("Invalid HTTP request method.");
+        }
+        
+        $encoded_data = 'cmd=_notify-validate';
+        
+        foreach ($_POST as $key => $value) {
+            if (get_magic_quotes_gpc() == 1) { 
+                $encoded_data .= "&$key=".urlencode(stripslashes($value));
+            } else {
+                $encoded_data .= "&$key=".urlencode($value);
+            }
+        }
+        
+        if ($this->use_curl) $this->curlPost($encoded_data); 
+        else $this->fsockPost($encoded_data);
+        
+        if (strpos($this->response_status, '200') === false) {
+            throw new Exception("Invalid response status: ".$this->response_status);
+        }
+        
+        if (strpos($this->response, "VERIFIED") !== false) {
+            return true;
+        } elseif (strpos($this->response, "INVALID") !== false) {
+            return false;
+        } else {
+            throw new Exception("Unexpected response from PayPal.");
+        }
+    }
+}
+?>
